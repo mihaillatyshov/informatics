12c86f96a6951275c4f219744cb1acf0ec7ee956
Михаил <mihail.latyshov2@mail.ru>
2020-05-17 19:49:11 +0300

    hw_03_dynamic_array_verification 2

--- hw_03_dynamic_array_verification/project1.pas
+++ hw_03_dynamic_array_verification/project1.pas
@@ -3,20 +3,28 @@ program project1;
 type
   TArray = array of Integer;
 
+
 function FindNE(const Arr : TArray; const val : Integer; s : Integer; e : Integer) : Integer;
-var CV : Integer;
+var
+  CV, ns, ne : Integer;
 begin
   CV := (s + e) div 2;
 
   if ((s = e) or (Arr[CV] = val)) then
     Exit(CV);
 
-  if (Arr[CV] > val) then
-    e := CV
-  else
-    s := CV;
+  ns := s;
+  ne := e;
 
-  Exit(FindNE(Arr, val, s, e));
+  if (Arr[CV] > val) then
+    ne := CV
+  else
+    ns := CV;
+
+  if ((ns = s) and (ne = e)) then
+    Exit(CV);
+
+  Exit(FindNE(Arr, val, ns, ne));
 end;
 
 function FindOne(const Arr1 : TArray; const Arr2 : TArray; const K : Integer) : Integer;
@@ -27,149 +35,121 @@ begin
     Exit(Arr2[K - Length(Arr1) - 1]);
 end;
 
-function FindTwo(const Arr1 : TArray; const Arr2 : TArray; const K : Integer) : Integer;
+function FindThr(const Arr1 : TArray; const Arr2 : TArray; const K : Integer) : Integer;
 var
   fs, fe : Integer;
-  Eq : Integer;
-  i, id : Integer;
-  LastVal, RidM, RidN, ValId : Integer;
+  TotEl : Integer;
+  id, Rid1, Rid2 : Integer;
+  LastVal : Integer;
+begin
+  fs := FindNE(Arr1, Arr2[0], 0, Length(Arr1) - 1);
+  while (Arr1[fs] > Arr2[0]) do
+    fs -= 1;
+
+  if ((K - 1) <= fs) then
+    Exit(Arr1[K - 1]);
+
+  fe := FindNE(Arr2, Arr1[Length(Arr1) - 1], 0, Length(Arr2) - 1);
+  while (Arr2[fe] < Arr1[Length(Arr1) - 1]) do
+    fe += 1;
+
+  TotEl := Length(Arr1) - fs + fe;
+          
+  WriteLn('Ban: ', fs, ' ', fe, ' ', K - Length(Arr1) - 1);
+  if ((K - 1) >= (fs + TotEl)) then
+    Exit(Arr2[K - Length(Arr1) - 1]);
+
+  WriteLn('Ban: ');
+
+  // -----------
+
+  Rid1 := fs + 1;
+  Rid2 := 0;
+  LastVal := Arr2[0];
+  id := fs + 1;
+
+  while (true) do
+  begin
+    if (Rid1 = Length(Arr1)) then
+      Exit(Arr2[Rid2 + K - id - 1]);
+
+    if (Rid2 = fe) then
+      Exit(Arr1[Rid1 + K - id - 1]);
+
+    id += 1;
+    if (Arr1[Rid1] < Arr2[Rid2]) then
+    begin 
+      LastVal := Arr1[Rid1];
+      Rid1 += 1;
+    end
+    else
+    begin                    
+      LastVal := Arr2[Rid2];
+      Rid2 += 1;
+    end;
+
+    if (id = K) then
+      Exit(LastVal);
+  end;
+end;
+
+function FindTwo(const Arr1 : TArray; const Arr2 : TArray; const K : Integer): Integer;
+var
+  fs, fe : Integer;
+  TotEl : Integer;
+  Disp1, Disp2 : Integer;
+  i, id, Rid1, Rid2 : Integer;
+  LastVal : Integer;
 begin
   fs := FindNE(Arr1, Arr2[0], 0, Length(Arr1) - 1);
   if (Arr1[fs] > Arr2[0]) then
     fs -= 1;
 
   if ((K - 1) <= fs) then
-    Exit(K - 1);
+    Exit(Arr1[K - 1]);
 
-  if Arr1[fs] <= Arr2[0] then
-  begin
-    LastVal := Arr1[fs];
-    RidM := fs + 1;
-    RidN := 0;
-  end
-  else
-  begin
-    LastVal := Arr2[0];
-    RidM := fs;
-    RidN := 1;
-  end;
-  ValId := fs + 1;
-
-  while ValId < K do
-  begin
-    if RidM = Length(Arr1) then
-    begin
-      if Arr2[RidN] > LastVal then
-        ValId += 1;
-      LastVal := Arr2[RidN];
-      RidN += 1;
-      continue;
-    end;
-
-    if RidN = Length(Arr2) then
-    begin
-      if Arr1[RidM] > LastVal then
-        ValId += 1;
-      LastVal := Arr1[RidM];
-      RidM += 1;
-      continue;
-    end;
-
-    if (Arr1[RidM] <= Arr2[RidN]) then
-    begin
-      if Arr1[RidM] > LastVal then
-        ValId += 1;
-      LastVal := Arr1[RidM];
-      RidM += 1;
-    end
-    else
-    begin
-      if Arr2[RidN] > LastVal then
-        ValId += 1;
-      LastVal := Arr2[RidN];
-      RidN += 1;
-    end;
-  end;
-
-  Exit(LastVal);
-
-  {
-  fe := FindNE(Arr2, Arr1[Length(Arr1) - 1], 0, Length(Arr2) - 1);
-  if (Arr2[fe] < Arr1[Length(Arr1) - 1]) then
+  fe := FindNe(Arr1, Arr2[Length(Arr2) - 1], fs, Length(Arr1) - 1);
+  if (Arr1[fe] < Arr2[Length(Arr2) - 1]) then
     fe += 1;
 
-  Eq := 0;
-  for i := fs to (Length(Arr1) - 1) do
+  if (K > (fe + Length(Arr2))) then
+    Exit(Arr1[K - Length(Arr2) - 1]);
+
+  // -----------
+
+  Rid1 := fs + 1;
+  Rid2 := 0;
+  LastVal := Arr2[0];
+  id := fs + 1;
+
+  while (true) do
   begin
-    id := FindNE(Arr2, Arr1[i], 0, fe);
-    if (Arr1[i] = Arr2[id]) then
-      Eq += 1;
-  end;
-  }
+    if (Rid1 = fs) then
+      Exit(Arr2[Rid2 + K - id - 1]);
 
+    if (Rid2 = Length(Arr2)) then
+      Exit(Arr1[Rid1 + K - id - 1]);
 
-
-end;
-
-function FindThr(const ArrM : TArray; const ArrN : TArray; const K : Integer): Integer;   
-var
-  LastVal, RidM, RidN, ValId, i : Integer;
-begin
-  if ArrM[0] <= ArrN[0] then
-  begin
-    LastVal := ArrM[0];
-    RidM := 1;
-    RidN := 0;
-  end
-  else
-  begin
-    LastVal := ArrN[0];
-    RidM := 0;
-    RidN := 1;
-  end;
-  ValId := 1;
-
-  while ValId < K do
-  begin
-    if RidM = Length(ArrM) then
+    id += 1;
+    if (Arr1[Rid1] < Arr2[Rid2]) then
     begin
-      if ArrN[RidN] > LastVal then
-        ValId += 1;
-      LastVal := ArrN[RidN];
-      RidN += 1;
-      continue;
-    end;
-
-    if RidN = Length(ArrN) then
-    begin
-      if ArrM[RidM] > LastVal then
-        ValId += 1;
-      LastVal := ArrM[RidM];
-      RidM += 1;
-      continue;
-    end;
-
-    if (ArrM[RidM] <= ArrN[RidN]) then
-    begin
-      if ArrM[RidM] > LastVal then
-        ValId += 1;
-      LastVal := ArrM[RidM];
-      RidM += 1;
+      LastVal := Arr1[Rid1];
+      Rid1 += 1;
     end
     else
     begin
-      if ArrN[RidN] > LastVal then
-        ValId += 1;
-      LastVal := ArrN[RidN];
-      RidN += 1;
+      LastVal := Arr2[Rid2];
+      Rid2 += 1;
     end;
+
+    if (id = K) then
+      Exit(LastVal);
   end;
 end;
 
 var
   M, N, K : Integer;
   ArrM, ArrN : TArray;
-  RidM, RidN, ValId : Integer;
   LastVal : Integer;
   i : Integer;
 
@@ -195,18 +175,22 @@ begin
   ReadLn(K);
 
   // Doing somthing with data...
-  if (ArrM[Length(ArrM) - 1] < ArrN[0]) then
+  if (Length(ArrN) = 0) then
+    LastVal := FindOne(ArrM, ArrN, K)
+  else if (Length(ArrM) = 0) then
+    LastVal := FindOne(ArrN, ArrM, K)
+  else if (ArrM[Length(ArrM) - 1] < ArrN[0]) then
     LastVal := FindOne(ArrM, ArrN, K)
   else if (ArrN[Length(ArrN) - 1] < ArrM[0]) then
     LastVal := FindOne(ArrN, ArrM, K)
   else if ((ArrM[0] >= ArrN[0]) and (ArrM[Length(ArrM) - 1] <= ArrN[Length(ArrN) - 1])) then
-    LastVal := FindTwo(ArrM, ArrN, K)
-  else if ((ArrN[0] >= ArrM[0]) and (ArrN[Length(ArrN) - 1] <= ArrM[Length(ArrM) - 1])) then
     LastVal := FindTwo(ArrN, ArrM, K)
+  else if ((ArrN[0] >= ArrM[0]) and (ArrN[Length(ArrN) - 1] <= ArrM[Length(ArrM) - 1])) then
+    LastVal := FindTwo(ArrM, ArrN, K)
   else if ((ArrM[0] <= ArrN[Length(ArrN) - 1]) and (ArrM[Length(ArrM) - 1] >= ArrN[Length(ArrN) - 1])) then
-    LastVal := FindThr(ArrM, ArrN, K)
+    LastVal := FindThr(ArrN, ArrM, K)
   else if ((ArrN[0] <= ArrM[Length(ArrM) - 1]) and (ArrN[Length(ArrN) - 1] >= ArrM[Length(ArrM) - 1])) then
-    LastVal := FindThr(ArrN, ArrM, K);
+    LastVal := FindThr(ArrM, ArrN, K);
 
 
   WriteLn('Found value: ', LastVal);
